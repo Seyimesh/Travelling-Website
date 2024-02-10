0bf5f0d88a9e6b2e3cc17ec856ff3120f3609555
SeyiMesh <seyimeshileya@gmail.com>
2024-02-10 21:42:08 +0100

    Fx: Modal

--- src/common/Modal/Modal.js
+++ src/common/Modal/Modal.js
@@ -1,7 +1,3 @@
 import React from "react";
-import { Modal } from "antd";
-import { Button } from "antd";
 
-const Modal = ({ children, title }) => {
-  return <Modal title={title}>{children}</Modal>;
-};
+
